e54a0a37d235f1877b7476e77d8acc5523697c4b
Samuel Oliveira <47918944+samuelc254@users.noreply.github.com>
2022-09-19 13:02:57 -0300

    add i2c

---
+++ i2c_controller/i2c_controller.ino
@@ -0,0 +1,18 @@
+#include <Wire.h>
+
+#define xPin A0
+#define yPin A1
+#define wPin A2
+
+void setup() {
+  Wire.begin();
+  pinMode(xPin,INPUT);
+}
+
+void loop() {
+  Wire.beginTransmission(12);
+  Wire.write(55);
+  Wire.write(100);
+  Wire.write(-10);
+  Wire.endTransmission();
+}
--- omni_robot/omni_robot.ino
+++ omni_robot/omni_robot.ino
@@ -1,8 +1,11 @@
+#include <Wire.h>
 #include "stepper.h"
 #include "motion.h"
 
 #define minVell 7000
 #define maxVell 500
+
+#define adress 12
 
 stepper bl_motor(2, 5, 8, minVell, maxVell);
 stepper fl_motor(3, 6, 8, minVell, maxVell);
@@ -11,27 +14,45 @@ stepper fr_motor(12, 13, 8, minVell, maxVell);
 
 omniRobot robot(&fl_motor, &fr_motor, &bl_motor, &br_motor);
 
-uint32_t lastMillis = 0,
+/*
+  uint32_t lastMillis = 0,
          radius = 200;
+*/
 
-int16_t angle = 0,
-        x = 0,
-        y = 0;
+int8_t x = 0,
+       y = 0,
+       w = 0;
 
 void setup()
 {
+  Wire.begin(adress);
+  Wire.onReceive(receiveEvent);
+
+  Serial.begin(9600);
 }
 
 void loop()
 {
+  /*
     if (millis() >= lastMillis + radius)
     {
-        angle++;
-        lastMillis = millis();
+     angle++;
+     lastMillis = millis();
     }
 
     x = sin((angle * 3.14) / 180) * 127;
     y = cos((angle * 3.14) / 180) * 127;
+  */
+  Serial.print(x);
+  Serial.print(y);
+  Serial.println(w);
+  robot.move(x, y, w);
+}
 
-    robot.move(x, y);
+void receiveEvent(int howMany) {
+  if (Wire.available()) {
+    x = Wire.read();
+    y = Wire.read();
+    w = Wire.read();
+  }
 }
