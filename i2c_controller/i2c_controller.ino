92a1f1ab280ea29b729f373523abeadeeff03537
Samuel Oliveira <47918944+samuelc254@users.noreply.github.com>
2022-09-22 10:45:40 -0300

    add analog deadzone

--- i2c_controller/i2c_controller.ino
+++ i2c_controller/i2c_controller.ino
@@ -1,9 +1,23 @@
 #include <Wire.h>
 
+#define DEBUG_LOG 0
+
+#if DEBUG_LOG == 1
+#define SerialBegin(x) Serial.begin(x)
+#define SerialPrint(x) Serial.print(String(x))
+#define SerialPrintln(x) Serial.println(String(x))
+#else
+#define SerialBegin(x)
+#define SerialPrint(x)
+#define SerialPrintln(x)
+#endif
+
 #define xPin A0
 #define yPin A1
 #define wPin A2
-#define led 13
+#define led 15
+
+#define deadzone 8
 
 int8_t x,
        y,
@@ -14,6 +28,7 @@ uint32_t lastBlink = 0;
 void setup()
 {
   Wire.begin();
+  SerialBegin(9600);
 
   pinMode(xPin, INPUT);
   pinMode(yPin, INPUT);
@@ -23,7 +38,7 @@ void setup()
 
 void loop()
 {
-  if (millis() > (lastBlink + 1000))
+  if (millis() > (lastBlink + 100))
   {
     digitalWrite(led, !digitalRead(led));
     lastBlink = millis();
@@ -32,6 +47,20 @@ void loop()
   x = map(analogRead(xPin), 0, 1023, -127, 127);
   y = map(analogRead(yPin), 0, 1023, -127, 127);
   w = map(analogRead(wPin), 0, 1023, -127, 127);
+
+  if ((x >= -deadzone) && (x <= deadzone))
+    x = 0;
+  if ((y >= -deadzone) && (y <= deadzone))
+    y = 0;
+  if ((w >= -deadzone) && (w <= deadzone))
+    w = 0;
+
+  SerialPrint("x");
+  SerialPrint(x);
+  SerialPrint(" y");
+  SerialPrint(y);
+  SerialPrint(" w");
+  SerialPrintln(w);
 
   Wire.beginTransmission(12);
   Wire.write(x);
