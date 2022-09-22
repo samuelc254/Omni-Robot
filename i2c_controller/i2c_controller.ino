96cb27adfd0b6f29d35c51d9eeaf088b4a2e3824
Samuel Oliveira <47918944+samuelc254@users.noreply.github.com>
2022-09-22 11:16:43 -0300

    add analog autocalibration

--- i2c_controller/i2c_controller.ino
+++ i2c_controller/i2c_controller.ino
@@ -19,9 +19,11 @@
 
 #define deadzone 8
 
-int8_t x,
-       y,
-       w;
+int8_t axis[3];
+
+uint16_t reads[3],
+         maxReads[3] = {0, 0, 0},
+         minReads[3] = {0, 0, 0};
 
 uint32_t lastBlink = 0;
 
@@ -44,27 +46,33 @@ void loop()
     lastBlink = millis();
   }
 
-  x = map(analogRead(xPin), 0, 1023, -127, 127);
-  y = map(analogRead(yPin), 0, 1023, -127, 127);
-  w = map(analogRead(wPin), 0, 1023, -127, 127);
+  reads[0] = analogRead(xPin);
+  reads[1] = analogRead(yPin);
+  reads[2] = analogRead(wPin);
 
-  if ((x >= -deadzone) && (x <= deadzone))
-    x = 0;
-  if ((y >= -deadzone) && (y <= deadzone))
-    y = 0;
-  if ((w >= -deadzone) && (w <= deadzone))
-    w = 0;
+  for (int i = 0; i < 3; i++)
+  {
+    if (reads[i] > maxReads[i])
+      maxReads[i] = reads[i];
+    if (reads[i] < minReads[i])
+      minReads[i] = reads[i];
+
+    axis[i] = map(reads[i], minReads[i], maxReads[i], -127, 127);
+
+    if ((axis[i] >= -deadzone) && (axis[i] <= deadzone))
+      axis[i] = 0;
+  }
 
   SerialPrint("x");
-  SerialPrint(x);
+  SerialPrint(axis[0]);
   SerialPrint(" y");
-  SerialPrint(y);
+  SerialPrint(axis[1]);
   SerialPrint(" w");
-  SerialPrintln(w);
+  SerialPrintln(axis[2]);
 
   Wire.beginTransmission(12);
-  Wire.write(x);
-  Wire.write(y);
-  Wire.write(w);
+  Wire.write(axis[0]); // axis X
+  Wire.write(axis[1]); // axis Y
+  Wire.write(axis[2]); // axis W
   Wire.endTransmission();
 }
